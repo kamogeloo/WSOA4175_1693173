4db1639f8a8c4e9e73ceec83f9319086472265ec
KamogeloMafaesa <60877078+kamogeloo@users.noreply.github.com>
2021-09-27 10:23:26 +0200

    added blogs section

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ hamburger.addEventListener("click", ()=>{
         link.classList.toggle("fade");
     });
 });
+
+//hover over gif
+const gif = document.querySelector('.giphy-embed');
+
+gif.addEventListener("mouseenter", (e) =>{
+    gif.style.transform = "translateZ(150px) translateX(-35px) translateY(20px)";
+    gif.style.filter = "drop-shadow(-1px -1px 12px red)";
+})
+
+gif.addEventListener("mouseleave", (e) =>{
+    gif.style.transform = "translateZ(0px)";
+    gif.style.filter = "none";
+})
