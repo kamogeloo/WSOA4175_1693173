cbe02fae0da613993fdf7de858f6ada56c7c3f35
KamogeloMafaesa <60877078+kamogeloo@users.noreply.github.com>
2021-10-03 14:27:12 +0200

    added images and made blog post pages

--- app.js
+++ app.js
@@ -11,15 +11,21 @@ hamburger.addEventListener("click", ()=>{
     });
 });
 
+//on click menu button change color
+document.getElementById('menuItem').onclick = function() {
+    document.links.style.color = "red"
+
+};
 //hover over gif
 const gif = document.querySelector('.giphy-embed');
 
 gif.addEventListener("mouseenter", (e) =>{
     gif.style.transform = "translateZ(150px) translateX(-35px) translateY(20px)";
-    gif.style.filter = "drop-shadow(-1px -1px 12px red)";
+    gif.style.filter = "drop-shadow(-1px -1px 8px gray)";
 })
 
 gif.addEventListener("mouseleave", (e) =>{
     gif.style.transform = "translateZ(0px)";
     gif.style.filter = "none";
 })
+
